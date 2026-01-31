bf2ebfec42b5d4675525cece8068866a598c5dec
Raghav <ra346238@gmail.com>
2026-01-31 14:35:00 -0800

    carousel added

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,3 +1,6 @@
+import '@mantine/core/styles.css'
+import '@mantine/carousel/styles.css'
+import { MantineProvider } from '@mantine/core'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +8,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
